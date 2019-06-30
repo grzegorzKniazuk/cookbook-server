e206c68e2a5665d581448899bfc62c170b2da2b1
Grzegorz Kniazuk <grzegorz.kniazuk@gmail.com>
2019-06-30 10:23:46 +0200

    29.06 cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ImagePath, ORIGIN } from './shared/constants';
+import { ImagePath } from './shared/constants';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    app.enableCors({ credentials: true, origin: ORIGIN, optionsSuccessStatus: 200 });
+    app.enableCors({ credentials: true, origin: true, optionsSuccessStatus: 200 });
     app.useStaticAssets(join(__dirname, '..', ImagePath));
 
     await app.listen(3000);
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './database-error-messages';
-export * from './origin';
 export * from './image-path';
