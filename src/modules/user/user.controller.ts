fc7da2f25c800cd55c0422fa4bce877a63b463a3
Grzegorz Kniazuk <grzegorz.kniazuk@gmail.com>
2019-07-07 20:16:49 +0200

    06.07 poprawki

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,4 +4,5 @@ import { FeatureName } from '../../shared/enums';
 @Controller(FeatureName.USER)
 export class UserController {
 
+
 }
---
+++ src/shared/data-transfer-objects/user-has-favorites.dto.ts
@@ -0,0 +1,4 @@
+export class UserHasFavoritesDto {
+    public readonly user_id: number;
+    public readonly recipe_id: number;
+}
